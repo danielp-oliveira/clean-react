c1a23c886273cf6cb28cd3dbffe2d39cf1ae36bb
danielp-oliveira <danielp.oliveira@outlook.com>
2020-06-08 08:36:05 -0300

    feat: prototyping login

---
+++ src/presentation/components/spinner/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Styles from './spinner-styles.scss'
+
+const Spinner: React.FC = () => (
+  <div className={Styles.spinner}>
+    <div/>
+    <div/>
+    <div/>
+    <div/>
+  </div>
+)
+
+export default Spinner
