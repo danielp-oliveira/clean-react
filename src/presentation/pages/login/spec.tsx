325b90223601544d1ca168c868b20366f623c6cd
danielp-oliveira <danielp.oliveira@outlook.com>
2020-06-08 23:25:34 -0300

    test: ensure login start with inputs with required state

--- src/presentation/pages/login/spec.tsx
+++ src/presentation/pages/login/spec.tsx
@@ -9,5 +9,11 @@ describe('Login Component', () => {
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBeTruthy()
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('\u{1F534}')
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('\u{1F534}')
   })
 })
