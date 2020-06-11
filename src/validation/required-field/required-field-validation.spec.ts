1536755ec220741c648878b3f51904ab2ad6507d
danielp-oliveira <danielp.oliveira@outlook.com>
2020-06-11 13:55:35 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFieldError } from '@/validation/errors'
 import { RequiredFiledValidation } from './required-filed-validation'
+import faker from 'faker'
 
 describe('RequiredFiledValidation', () => {
   test('Should return error if field is empty', () => {
     const sut = new RequiredFiledValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return falsy if field is not empty', () => {
+    const sut = new RequiredFiledValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
